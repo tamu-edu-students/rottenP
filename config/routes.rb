b96a78ab14cbcae83e9362ddeff0928d949b0abc
gdbrowne85 <gdbrowne85@tamu.edu>
2024-02-18 15:07:34 -0600

    replaced default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # Other routes...
+
+  # Set the root route to the index action of MoviesController
+  root 'movies#index'
+
   resources :movies do
     collection do
       get 'sort'
